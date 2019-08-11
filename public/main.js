0eb539f238ac1620b26fb54ff8ff46c31a244438
NorbertLam <noobertlm@gmail.com>
2019-08-11 16:18:04 -0400

    add router and clipcontainer

--- public/main.js
+++ public/main.js
@@ -7,10 +7,17 @@ const url = require('url');
 const isDev = require('electron-is-dev');
 
 let mainWindow;
+let clipWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({width: 900, height: 680});
+  clipWindow = new BrowserWindow({width: 1280, height: 720, parent: mainWindow, show: true});
+
+
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow.loadURL(isDev ? 'http://localhost:3000/clip' : `file://${path.join(__dirname, '../build/index.html')}`);
+
+
   mainWindow.on('closed', () => mainWindow = null);
 }
 
---
+++ src/containers/ClipContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ClipContainer extends React.Component {
+
+  render() {
+
+    return(
+      <div>
+        <h1>hello</h1>
+      </div>
+    )
+  }
+}
+
+export default ClipContainer;
